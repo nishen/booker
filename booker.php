ee1cf5e5388d77b32073980735ba24616aab4f23
nishen <nish.naidoo@gmail.com>
2015-05-28 00:24:11 +1000

    Work in progress...

--- booker.php
+++ booker.php
@@ -6,3 +6,16 @@
  * Time: 11:41 PM
  */
 
+
+$content = file_get_contents("login-page.html");
+print getToken($content);
+
+
+function getToken($content)
+{
+    $res = preg_match('/name=\"token\" +value=\"(.*)\"/', $content, $matches);
+    if ($res > 0)
+        return $matches[1];
+
+    return "";
+}
