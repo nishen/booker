466c241c4de1f6840620978f06b4b4b7ba056e63
nishen <nish.naidoo@gmail.com>
2015-05-31 23:50:49 +1000

    Work in progress...

--- booker.php
+++ booker.php
@@ -6,14 +6,35 @@
  * Time: 11:41 PM
  */
 
+require_once(__DIR__ . '/httpful.phar');
 
-$content = file_get_contents("login-page.html");
-print getToken($content);
+use \Httpful\Httpful;
+use \Httpful\Http;
+use \Httpful\Mime;
+use \Httpful\Proxy;
+use \Httpful\Response;
+use \Httpful\Request;
 
+$siteUrl = 'https://secure.activecarrot.com/login?site=382';
+
+
+$res = Request::get($siteUrl)->send();
+$token = getToken($res->body);
+
+if ($token == "")
+{
+    echo "failed to obtain token...";
+    exit(1);
+}
+
+echo "$token\n";
+
+#$content = file_get_contents("login-page.html");
+#print getToken($content);
 
 function getToken($content)
 {
-    $res = preg_match('/name=\"token\" +value=\"(.*)\"/', $content, $matches);
+    $res = preg_match('/name=\"atoken\" +value=\"(.*)\"/', $content, $matches);
     if ($res > 0)
         return $matches[1];
 
